44c5037330537f9f5c1342a22dc318ac59029cbb
Ryan Quinn <ghost@Ryans-MacBook-Pro.local>
2023-10-20 02:26:14 -0700

    0003

--- api/db/migrations/20231020092559_real_production_on_railway_time_for_chaser/migration.sql
+++ api/db/migrations/20231020092559_real_production_on_railway_time_for_chaser/migration.sql
@@ -1,26 +1,32 @@
 -- CreateTable
 CREATE TABLE "UserExample" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT
+    "name" TEXT,
+
+    CONSTRAINT "UserExample_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "PlanechaseImage" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL DEFAULT '',
     "encodedString" TEXT NOT NULL DEFAULT '',
     "planechaseCardId" TEXT,
-    CONSTRAINT "PlanechaseImage_planechaseCardId_fkey" FOREIGN KEY ("planechaseCardId") REFERENCES "PlanechaseCard" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+    CONSTRAINT "PlanechaseImage_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "PlanechaseCard" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL DEFAULT '',
     "plane" TEXT NOT NULL DEFAULT '',
     "abilityDescription" TEXT NOT NULL DEFAULT '',
     "chaosAbilityDescription" TEXT NOT NULL DEFAULT '',
-    "planechaseImageId" TEXT
+    "planechaseImageId" TEXT,
+
+    CONSTRAINT "PlanechaseCard_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -28,3 +34,6 @@ CREATE UNIQUE INDEX "UserExample_email_key" ON "UserExample"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "PlanechaseImage_planechaseCardId_key" ON "PlanechaseImage"("planechaseCardId");
+
+-- AddForeignKey
+ALTER TABLE "PlanechaseImage" ADD CONSTRAINT "PlanechaseImage_planechaseCardId_fkey" FOREIGN KEY ("planechaseCardId") REFERENCES "PlanechaseCard"("id") ON DELETE SET NULL ON UPDATE CASCADE;
