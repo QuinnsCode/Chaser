db48f027204304e9e6ed3b07a2c6587603eda0f7
Ryan Quinn <ghost@Ryans-MacBook-Pro.local>
2023-10-23 21:07:26 -0700

    015

--- web/src/layouts/DefaultLayout/DefaultLayout.jsx
+++ web/src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -1,33 +1,53 @@
 import { useRef, useEffect } from 'react'
 
 // import { Link, routes } from '@redwoodjs/router'
-import { useAuth } from '@clerk/clerk-react'
-
+import { useAuth } from 'src/auth'
 import FloatingMenu from 'src/components/FloatingMenu/FloatingMenu'
 
 const DefaultLayout = ({ children }) => {
   const { currentUser } = useAuth()
 
+  const id = currentUser?.id
+
+  console.log({ id })
+
   const currentUserRef = useRef('')
 
   useEffect(() => {
     if (currentUser) {
+      console.log({ currentUser })
       currentUserRef.current = currentUser
     }
   }, [currentUser])
 
   return (
     <>
-      <header></header>
+      <header>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to={routes.dashboard()}>Dashboard</Link>
+            </li>
+            <li>
+              <Link to={routes.game()}>Game</Link>
+            </li>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+          </ul>
+        </nav> */}
+      </header>
       <hr />
-      <div className="w-full">
-        <FloatingMenu currentUser={currentUser} />
-        <main>
-          <div className="border-2 border-solid border-black text-black">
-            {children}
-          </div>
-        </main>
-      </div>
+      {currentUser?.id && (
+        <>
+          <FloatingMenu currentUser={currentUser} />
+          <main>
+            <div className="border-2 border-solid border-black text-black">
+              {children}
+            </div>
+          </main>
+        </>
+      )}
     </>
   )
 }
