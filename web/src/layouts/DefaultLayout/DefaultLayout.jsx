f242a73b0de7a127753af3a3305348c93bf52226
Ryan Quinn <ghost@Ryans-MacBook-Pro.local>
2023-10-23 21:05:09 -0700

    014

--- web/src/layouts/DefaultLayout/DefaultLayout.jsx
+++ web/src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -8,8 +8,6 @@ import FloatingMenu from 'src/components/FloatingMenu/FloatingMenu'
 const DefaultLayout = ({ children }) => {
   const { currentUser } = useAuth()
 
-  const id = currentUser?.id
-
   const currentUserRef = useRef('')
 
   useEffect(() => {
@@ -22,18 +20,14 @@ const DefaultLayout = ({ children }) => {
     <>
       <header></header>
       <hr />
-      {currentUser?.id ? (
-        <div className="w-full">
-          <FloatingMenu currentUser={currentUser} />
-          <main>
-            <div className="border-2 border-solid border-black text-black">
-              {children}
-            </div>
-          </main>
-        </div>
-      ) : (
-        <></>
-      )}
+      <div className="w-full">
+        <FloatingMenu currentUser={currentUser} />
+        <main>
+          <div className="border-2 border-solid border-black text-black">
+            {children}
+          </div>
+        </main>
+      </div>
     </>
   )
 }
