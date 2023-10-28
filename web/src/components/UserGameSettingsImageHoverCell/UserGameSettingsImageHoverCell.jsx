e251d2f907338c0b65352e47ef23bbf5805431fe
Ryan Quinn <ghost@Ryans-MacBook-Pro.local>
2023-10-27 20:06:21 -0700

    018

--- web/src/components/UserGameSettingsImageHoverCell/UserGameSettingsImageHoverCell.jsx
+++ web/src/components/UserGameSettingsImageHoverCell/UserGameSettingsImageHoverCell.jsx
@@ -24,5 +24,6 @@ export const Failure = ({ error }) => (
 )
 
 export const Success = ({ card, closeMe }) => {
-  return <InspectCardHoverUIPanel card={card} closeMe={closeMe} />
+  // return <InspectCardHoverUIPanel card={card} closeMe={closeMe} />
+  return <div>{JSON.stringify(card)}</div>
 }
